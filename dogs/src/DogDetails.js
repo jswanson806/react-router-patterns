c8b7dfc2e44e424068379d1d5ed181362ce678d5
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2023-04-16 07:56:48 -0500

    refactored dog app and moved routes into their own component. Added code comments throughout dog app. Fixed bug where user was not redirected when invalid dog name was passed in the url

--- dogs/src/DogDetails.js
+++ dogs/src/DogDetails.js
@@ -1,29 +1,50 @@
 import React from "react";
-import {useParams, NavLink} from "react-router-dom";
+import { v4 as uuid } from "uuid";
+import {useParams, NavLink, useHistory} from "react-router-dom";
 import "./DogDetails.css"
 
+
+/**
+ * Renders details about a particular dog
+ * 
+ * Contains link back to the list of all dogs
+ */
+
 const DogDetails = ({dogs}) => {
+    // initialize history
+    const history = useHistory();
 
     // get the name from the URL
     const dogName = useParams().name;
+
     // find the corresponding dog in the dogs array within defaultProps, case insensitive
     const dog = dogs.find(dog => dog.name.toLowerCase() === dogName.toLowerCase());
 
+    // check for dog to exist in list of dogs
+    if(!dog){
+        // if dog doesn't exist, stop the render and redirect to list of all dogs
+        history.push('/dogs')
+        return null;
+    }
+
     return(
         <div className="DogDetails">
+            {/* dog image */}
             <div className="DogDetails-image-container">
-                <img src={dog.src} alt={`${dog.name}`}></img>
-            
+                <img src={dog.src} alt={`${dog.name}`}/>
+                {/* dog information */}
             <div className="DogDetails-info">
                 <h3>Name: {dog.name}</h3>
                 <h5>Age: {dog.age}</h5>
+                {/* map dog facts to li's */}
                 <ul>
                     {dog.facts.map(fact => 
-                        <li>{fact}</li>
+                        <li key={uuid()}>{fact}</li>
                     )}
                 </ul>
             </div>
             </div>
+            {/* nav link to return to list of dogs */}
             <div className="DogDetails-nav">
                 <nav>
                     <NavLink exact to='/'>Go Back</NavLink>
--- dogs/src/DogList.js
+++ dogs/src/DogList.js
@@ -2,18 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./DogList.css"
 
-const DogList = ({dogs}) => {
 
+/**
+ * Renders list of all dogs
+ * 
+ * Displays an image and link to the dog details route
+ */
+
+const DogList = ({dogs}) => {
+    
 
     return(
         <div className="DogList">
             <div className="DogList-dogs">
                 <h1>Dogs</h1>
                 <nav>
+                    {/* map dog's image src to img and dog's name to NavLink */}
                     {dogs.map(dog => 
-                        <div className="DogList-choices">
+                        <div className="DogList-choices" key={dog.name}>
                             <img src={dog.src} alt={dog.name}></img>
-                            <NavLink exact to={`/dogs/${dog.name}`} key={dog.name}>
+                            <NavLink exact to={`/dogs/${dog.name}`} >
                                 {dog.name}
                             </NavLink>
                         </div>
